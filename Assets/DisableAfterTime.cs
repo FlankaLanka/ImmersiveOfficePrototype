1ab493f204ef9a11edfabca64e2732db9fe9b0fb
frankyang233 <frankyang233@gmail.com>
2022-08-23 10:44:03 -0700

    polish

--- Assets/DisableAfterTime.cs
+++ Assets/DisableAfterTime.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class DisableAfterTime : MonoBehaviour
 {
+    [Header("Story")]
+    [SerializeField] private Flowchart f;
+
+    [Header("Audio")]
     [SerializeField] private AudioClip fixedBackgroundMusic;
     [SerializeField] private AudioSource backgroundMusic;
     private readonly float sec = 7f;
@@ -12,12 +17,13 @@ public class DisableAfterTime : MonoBehaviour
         StartCoroutine(TimedDisable(sec));
     }
 
-    //after visuals, change audio to fixed
+    //after visuals, change audio to good bgm and start story
     private IEnumerator TimedDisable(float sec)
     {
         yield return new WaitForSeconds(sec);
         backgroundMusic.clip = fixedBackgroundMusic;
         backgroundMusic.Play();
+        f.SetBooleanVariable("RetrieveDataDone", true);
         gameObject.SetActive(false);
     }
 }
